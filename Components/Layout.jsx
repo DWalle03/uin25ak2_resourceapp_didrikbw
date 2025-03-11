98eb8e5c14a7e10996cfe55d0b76e0ae6b17b9d0
DWalle03 <didrikbunes@outlook.com>
2025-03-11 11:53:48 +0100

    HTML inn i Layout komponentog diverse

---
+++ Components/Layout.jsx
@@ -0,0 +1,25 @@
+export default function Layout() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <button id="HTML">HTML</button>
+          </li>
+          <li>
+            <button id="CSS">CSS</button>
+          </li>
+          <li>
+            <button id="JavaScript">Javascript</button>
+          </li>
+          <li>
+            <button id="React">React</button>
+          </li>
+          <li>
+            <button id="Sanity">Sanity and headless CMS</button>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
